d260044f3ab6e453e6858253cf0f633ccbf36d5f
Matias Garcia <95774271+matiasgarcia-dot@users.noreply.github.com>
2025-08-19 14:09:54 -0300

    Agregando Clase 02 Profundizando en Python con Sistemas Numéricos -> Parte 1

---
+++ Python/Clase 2/tipo_float.py
@@ -0,0 +1,25 @@
+# Profundizando en el tipo float
+a= 3.0
+
+
+# Contructor de tipo float -> puede recibir int y str
+a= float(10)
+a= float('10')
+print(f"a: {a:.2f}")
+
+# Notacion exponencial( valores positivos o negativos)
+a=3e5
+print(f"a: {a:.2f}")
+
+a=3e-5
+print(f"a: {a:.5f}")
+
+# Cuealquier calculo que incluye un float, todo cambia a float
+
+a = 4.0 + 5
+
+print(a)
+print(type(a))
+
+
+
