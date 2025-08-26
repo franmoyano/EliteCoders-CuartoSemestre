5fab08bddc2586cd73623242c3cc94cc75126aaa
Manolo Bassot <manolo.bassot97@gmail.com>
2025-08-26 19:19:37 -0300

    Clase 3 Python y JS

--- E-commerce/client/js/products.js
+++ E-commerce/client/js/products.js
@@ -1,7 +1,7 @@
 const products = [
-  { id: 1, name: "Auto",   price: 1200, quanty: 10,  img: "client/media/img/auto.jpg"   },
-  { id: 2, name: "Moto",   price: 1800, quanty: 100, img: "client/media/img/moto.jpg"   },
-  { id: 3, name: "Cuatri", price: 500,  quanty: 140, img: "client/media/img/cuatri.jpg" },
-  { id: 4, name: "Bici",   price: 200,  quanty: 120, img: "client/media/img/bici.jpg"   }
+  { id: 1, name: "Auto",   price: 1200, quanty: 1,  img: "media/img/auto.jpg"   },
+  { id: 2, name: "Moto",   price: 1800, quanty: 1, img: "media/img/moto.jpg"   },
+  { id: 3, name: "Cuatri", price: 500,  quanty: 1, img: "media/img/cuatri.jpg" },
+  { id: 4, name: "Bici",   price: 200,  quanty: 1, img: "media/img/bici.jpg"   }
 ];
 
---
+++ Python/Clase 2/tipo-str.py
@@ -0,0 +1,11 @@
+#Profundizando en el tipo String
+#Concatenación automática en Python
+import math
+
+variable = ' Adios'
+mensaje = 'Hola ''Alumnos' + variable
+mensaje += ', Terminamos'
+print(mensaje)
+
+#Usamos la clase help para ayuda o documentacion (built-in)
+help(math.isnan)
