da1780b82b7f096a5e71f62e623a9e776128c0bd
Matias Garcia <95774271+matiasgarcia-dot@users.noreply.github.com>
2025-08-30 15:54:41 -0300

    Agregando Clase Programacion Miercoles IV Java,Node y Juego

---
+++ Node/01-Hola-Mundo/app2.js
@@ -0,0 +1,4 @@
+const saludar=(nombre) =>{// Funcion de flecha
+    return 'Saludos de ${nombre}'; // usamos back tic s
+}// esto es igual a: 'saludos'+ nombre;
+console.log(saludar('Matias'));
---
+++ Node/01-Hola-Mundo/app3.js
@@ -0,0 +1,15 @@
+console.log('Inicio del programa')
+
+setTimeout(() => {
+    console.log('Primer Timeout')
+}, 3000);
+
+setTimeout(() => {
+    console.log('Primer Segundo')
+}, 3000);
+
+setTimeout(() => {
+    console.log('Primer Tercero')
+}, 3000);
+
+console.log('Fin del programa');
