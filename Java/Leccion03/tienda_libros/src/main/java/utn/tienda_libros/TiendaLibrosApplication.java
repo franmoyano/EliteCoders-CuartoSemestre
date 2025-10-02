0c48fc8ca0f83a09ee9e14c3f3d1e467c100e064
Matias Garcia <95774271+matiasgarcia-dot@users.noreply.github.com>
2025-10-02 18:18:33 -0300

    Clase 08 Proyecto Tienda de Libros con SpringBoot y Swing -> Parte 03

--- Java/Leccion03/tienda_libros/src/main/java/utn/tienda_libros/TiendaLibrosApplication.java
+++ Java/Leccion03/tienda_libros/src/main/java/utn/tienda_libros/TiendaLibrosApplication.java
@@ -1,13 +1,29 @@
 package utn.tienda_libros;
 
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.ConfigurableApplicationContext;
+
+import java.awt.EventQueue;
 
 @SpringBootApplication
 public class TiendaLibrosApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TiendaLibrosApplication.class, args);
-	}
-
-}
+		
+        ConfigurableApplicationContext= contextoSpring = new SpringApplicationBuilder(TiendaLibrosApplication.class)
+                .headless(false)
+                .web(WebApplicationType.NONE)
+                .run(args);
+        // Ejecutamos el codigo para cargar el formulario
+        EventQueve.invokeLater(() -> { // Metodo labda 
+            //Obtenemos el objeto from a traves del spring
+            LibroFrom libroFrom= contextoSpring.getBean(LibroFrom.class);
+            libroFrom.setVisible(true);
+        })
+                
+                
+                
+ }
--- Java/Leccion03/tienda_libros/src/main/java/utn/tienda_libros/vista/LibroForm.java
+++ Java/Leccion03/tienda_libros/src/main/java/utn/tienda_libros/vista/LibroForm.java
@@ -5,13 +5,16 @@ import org.springframework.stereotype.Component;
 import utn.tienda_libros.servicio.LibroServicio;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 
 @Component
 public class LibroForm extends JFrame {
 
-    LibroServicio servicio;
+    LibroServicio libroServicio;
     private JPanel panel;
-
+    private JPanel tablaLibros;
+    private DefaultTableModel tablaModeloLibros;
+    
     @Autowired
     public LibroForm(LibroServicio libroServicio) {
         this.servicio = libroServicio;
@@ -23,5 +26,41 @@ public class LibroForm extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
         setSize(900, 700);
+        //Para obtener las dimenciones de la ventana
+        ToolKit toolkit = ToolKit.getDefaultTookit();
+        
+        Dimension tamaioPantalla = toolkit.getScreenSize();
+        int x = (tamanioPantalla.width - getWidth()/2);
+        int y = (tamanioPantalla.width - getWidth()/2);
+        setLocation(x,y);
+    }
+    
+    private void createUIComponents(){
+        this.tablaModeloLibros= new DefaultTableModel(0,5);
+        String[] cabecera = {"id","Libro","Autor","Precio","Existencias"};
+        this.tablaModeloLibros.setColumnIdentifiers(cabecera);
+        //Insstanciar el objeto de JTable
+        this.tablaLibros= new JTable(tablaModeloLibros);
+        listarLibros();
+        
+    }
+    
+    private void listarLibros(){
+        // Limpiar la tabla
+        tablaModeloLibros.setRowCount(0);
+        //Obtener los libros de la BD
+        var libros=libroServicio.listarLibros();
+        //Iteramos cada libro
+        libros.forEach((libro)->{// Funcion lambda
+            //Creammos cada registro para agregarlos a la tabla
+            Object[] renglonLibro= {
+                libro.getIdLibro(),
+                libro.getNombreLibro(),
+                libro.getAutor(),
+                libro.getPrecio(),
+                libro.getExistencias()
+            };
+            this.tablaModeloLibros.addRow(renglonLibro);
+        });
     }
 }
